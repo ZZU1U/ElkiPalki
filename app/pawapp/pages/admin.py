907901e3643bffdecd13a05e7c7ebc1e15612d8e
NikolaNE <kolanesov2858@gmail.com>
2024-04-27 16:06:25 +0600

    wewq

--- app/pawapp/pages/admin.py
+++ app/pawapp/pages/admin.py
@@ -1,9 +1,13 @@
 from flet import Text, Page
 import flet as ft
+import asyncio
 from ..components.appbar import MyAppBar
-
+from ..pages.adminchoices import adminchoice
 
 async def AdminPage(page: Page, back=None):
+    def runchoice(e):
+        asyncio.run(adminchoice(page, AdminPage))
+        page.update()
     page.clean()
     page.appbar = MyAppBar(title='Панель админа', back=back)
     page.appbar.center_title = True
@@ -18,7 +22,8 @@ async def AdminPage(page: Page, back=None):
     btn3 = ft.FilledButton(text='Добавить животное',
                            width=220,
                            height=45,
-                           style=ft.ButtonStyle(bgcolor='#FA7800'))
+                           style=ft.ButtonStyle(bgcolor='#FA7800'), on_click=runchoice)
+
     page.auto_scroll = False
     page.add(ft.Container(ft.Column(controls=[btn1, btn2, btn3], alignment=ft.MainAxisAlignment.CENTER,
                                    horizontal_alignment=ft.CrossAxisAlignment.CENTER), width=float('inf'), height=600))

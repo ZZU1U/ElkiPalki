4534da76ae99116b1df5301ffe2d9263db003aef
NikolaNE <kolanesov2858@gmail.com>
2024-04-27 23:58:58 +0600

    wewq

--- app/pawapp/pages/admin.py
+++ app/pawapp/pages/admin.py
@@ -3,8 +3,12 @@ import flet as ft
 import asyncio
 from ..components.appbar import MyAppBar
 from ..pages.adminchoices import adminchoice
+from ..pages.TablePageAdmin import tablepageadmin
 
 async def AdminPage(page: Page, back=None):
+
+    async def walksadmin(e):
+        await tablepageadmin(page, back=lambda e: asyncio.run(AdminPage(page, back)))
     def runchoice(e):
         asyncio.run(adminchoice(page, lambda e: asyncio.run(AdminPage(page, back))))
         page.update()
@@ -14,16 +18,14 @@ async def AdminPage(page: Page, back=None):
     btn1 = ft.FilledButton(text='Список прогулок',
                            width=220,
                            height=45,
-                           style=ft.ButtonStyle(bgcolor='#FA7800'))
-    btn2 = ft.FilledButton(text='Корма',
-                           width=220,
-                           height=45,
-                           style=ft.ButtonStyle(bgcolor='#FA7800'))
+                           style=ft.ButtonStyle(bgcolor='#FA7800'),
+                           on_click=walksadmin)
+
     btn3 = ft.FilledButton(text='Управление',
                            width=220,
                            height=45,
                            style=ft.ButtonStyle(bgcolor='#FA7800'), on_click=runchoice)
 
     page.auto_scroll = False
-    page.add(ft.Container(ft.Column(controls=[btn1, btn2, btn3], alignment=ft.MainAxisAlignment.CENTER,
+    page.add(ft.Container(ft.Column(controls=[btn1, btn3], alignment=ft.MainAxisAlignment.CENTER,
                                    horizontal_alignment=ft.CrossAxisAlignment.CENTER), width=float('inf'), height=600))
